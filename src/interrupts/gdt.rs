6cb5f7a5af08f053c24f58780d334e2911cdedfa
Nabil Elqatib <nabilelqatib@gmail.com>
2024-12-01 20:59:19 +0100

    Remove unsafe block and use addr_of macro

--- src/interrupts/gdt.rs
+++ src/interrupts/gdt.rs
@@ -12,7 +12,7 @@ lazy_static! {
             const STACK_SIZE: usize = 4 * 1024 * 5;
             static mut STACK: [u8; STACK_SIZE] = [0; STACK_SIZE];
 
-            let stack_start = VirtAddr::from_ptr(unsafe { &STACK });
+            let stack_start = VirtAddr::from_ptr(core::ptr::addr_of!(STACK));
             let stack_end = stack_start + STACK_SIZE;
             stack_end
         };
